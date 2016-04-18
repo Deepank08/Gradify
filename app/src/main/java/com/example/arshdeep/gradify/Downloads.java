28ad2ff142cf41eb6a2c55a6ec10e3b3a78f11b4
Varun Dhall <varun.dhall@studentpartner.com>
2016-04-19 00:18:04 +0530

    Added Download and View PDF features

--- app/src/main/java/com/example/arshdeep/gradify/Downloads.java
+++ app/src/main/java/com/example/arshdeep/gradify/Downloads.java
@@ -1,10 +1,18 @@
 package com.example.arshdeep.gradify;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.arshdeep.gradify.adapters.ItemClickListener;
@@ -12,17 +20,29 @@ import com.example.arshdeep.gradify.adapters.Section;
 import com.example.arshdeep.gradify.adapters.SectionedExpandableLayoutHelper;
 import com.example.arshdeep.gradify.models.Item;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Downloads extends AppCompatActivity implements ItemClickListener {
 
     RecyclerView mRecyclerView;
+    SessionManager session;
+    String shared_pref_data ;
+    CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        session = new SessionManager(getApplicationContext());
+        HashMap<String,String> user = session.getUserDetails();
+        shared_pref_data = user.get(SessionManager.KEY_ROLL);
+        shared_pref_data = shared_pref_data.substring(0,2);
+        coordinatorLayout=(CoordinatorLayout)findViewById(R.id.coordinatorLayout);
 
         //setting the recycler view
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
@@ -31,31 +51,31 @@ public class Downloads extends AppCompatActivity implements ItemClickListener {
 
         //random data
         ArrayList<Item> arrayList = new ArrayList<>();
-        arrayList.add(new Item("iPhone", 0));
-        arrayList.add(new Item("iPad", 1));
-        arrayList.add(new Item("iPod", 2));
-        arrayList.add(new Item("iMac", 3));
-        sectionedExpandableLayoutHelper.addSection("Apple Products", arrayList);
+        arrayList.add(new Item("CSE", 0, "http://gradify3.azurewebsites.net/Syllabus/" + shared_pref_data + "btechcse.pdf"));
+        arrayList.add(new Item("iPad", 1, ""));
+        arrayList.add(new Item("iPod", 2, ""));
+        arrayList.add(new Item("iMac", 3, ""));
+        sectionedExpandableLayoutHelper.addSection("Syllabus", arrayList);
         arrayList = new ArrayList<>();
-        arrayList.add(new Item("LG", 0));
-        arrayList.add(new Item("Apple", 1));
-        arrayList.add(new Item("Samsung", 2));
-        arrayList.add(new Item("Motorola", 3));
-        arrayList.add(new Item("Sony", 4));
-        arrayList.add(new Item("Nokia", 5));
+        arrayList.add(new Item("LG", 0, ""));
+        arrayList.add(new Item("Apple", 1, ""));
+        arrayList.add(new Item("Samsung", 2, ""));
+        arrayList.add(new Item("Motorola", 3, ""));
+        arrayList.add(new Item("Sony", 4, ""));
+        arrayList.add(new Item("Nokia", 5, ""));
         sectionedExpandableLayoutHelper.addSection("Companies", arrayList);
         arrayList = new ArrayList<>();
-        arrayList.add(new Item("Chocolate", 0));
-        arrayList.add(new Item("Strawberry", 1));
-        arrayList.add(new Item("Vanilla", 2));
-        arrayList.add(new Item("Butterscotch", 3));
-        arrayList.add(new Item("Grape", 4));
+        arrayList.add(new Item("Chocolate", 0, ""));
+        arrayList.add(new Item("Strawberry", 1, ""));
+        arrayList.add(new Item("Vanilla", 2, ""));
+        arrayList.add(new Item("Butterscotch", 3, ""));
+        arrayList.add(new Item("Grape", 4, ""));
         sectionedExpandableLayoutHelper.addSection("Ice cream", arrayList);
 
         sectionedExpandableLayoutHelper.notifyDataSetChanged();
 
         //checking if adding single item works
-        sectionedExpandableLayoutHelper.addItem("Ice cream", new Item("Tutti frutti",5));
+        sectionedExpandableLayoutHelper.addItem("Ice cream", new Item("Tutti frutti",5, ""));
         sectionedExpandableLayoutHelper.notifyDataSetChanged();
     }
 
@@ -83,10 +103,72 @@ public class Downloads extends AppCompatActivity implements ItemClickListener {
 
     @Override
     public void itemClicked(Item item) {
-        Toast.makeText(this, "Item: " + item.getName() + " clicked", Toast.LENGTH_SHORT).show();
+        //Item clicked - download the pdf and open it via default PDF Application
+        //now we have two cases 1. either file already exists 2. first time download
+
+        //if file already exists then just open it
+        String data = item.getData();
+        String name = item.getName();
+        name = name + ".pdf";
+        File pdfFile = new File(Environment.getExternalStorageDirectory() + "/Gradify/" + name);
+        if (pdfFile.exists())
+        {
+            //PDF file found display it
+            Uri path = Uri.fromFile(pdfFile);
+            Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
+            pdfIntent.setDataAndType(path, "application/pdf");
+            pdfIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            try {
+                startActivity(pdfIntent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(this, "No Application available to view PDF", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+        {
+            //Downloading the PDF file
+            Toast.makeText(this, "Downloading ..." , Toast.LENGTH_SHORT).show();
+
+
+            Snackbar snackbar = Snackbar
+                    .make(coordinatorLayout, "This is Awesome", Snackbar.LENGTH_LONG);
+
+            snackbar.show();
+
+
+
+
+            new DownloadFile().execute(data, name);
+        }
     }
 
+    private class DownloadFile extends AsyncTask<String, Void, Void> {
 
+        @Override
+        protected Void doInBackground(String... strings) {
+            String fileUrl = strings[0];
+            String fileName = strings[1];
+            String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
+            File folder = new File(extStorageDirectory, "Gradify");
+            folder.mkdir();
+            File pdfFile = new File(folder, fileName);
+            try{
+                pdfFile.createNewFile();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            FileDownloader.downloadFile(fileUrl, pdfFile);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+
+            Toast.makeText(getApplicationContext(), "Download Completed!" , Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public void itemClicked(Section section) {
--- app/src/main/java/com/example/arshdeep/gradify/models/Item.java
+++ app/src/main/java/com/example/arshdeep/gradify/models/Item.java
@@ -7,10 +7,12 @@ public class Item {
 
     private final String name;
     private final int id;
+    private final String data;
 
-    public Item(String name, int id) {
+    public Item(String name, int id, String data) {
         this.name = name;
         this.id = id;
+        this.data = data;
     }
 
     public int getId() {
@@ -20,4 +22,6 @@ public class Item {
     public String getName() {
         return name;
     }
+
+    public String getData() { return data;}
 }
